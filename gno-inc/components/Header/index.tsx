a04dd278d682177c8dfaca88d1bf012a3d15e849
Nina Horne <nina.horne1221@gmail.com>
2022-06-22 13:03:37 -0400

    header and init

---
+++ gno-inc/components/Header/index.tsx
@@ -0,0 +1,46 @@
+import Link from 'next/link';
+
+interface HeaderProps {}
+
+const Header: React.FC<HeaderProps> = () => {
+  return (
+    <header>
+      <div className="container mx-auto flex items-center justify-between p-4">
+        <div className="h-20">
+          <img
+            className="h-full"
+            src="images/gno-inc-logo@2x.png"
+            alt="GNO Inc logo"
+          />
+        </div>
+        <nav className="flex-1 mx-8">
+          <ul className="flex flex-row items-center w-full justify-around">
+            <li>
+              <Link href="#">
+                <a className="text-darkBlue font-bold mx-2" href="#">
+                  About GNO Inc.
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                <a className="text-darkBlue font-bold mx-2" href="#">
+                  Resource Hub
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                <a className="text-darkBlue font-bold mx-2" href="#">
+                  Login
+                </a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
